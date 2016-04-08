0a9ac3822095513089fbd5cd67123fc82635fc81
Dylan George Hsu <dghsu@mit.edu>
2016-04-08 06:21:42 -0400

    fix condition in which cut and count is used (check sig/background are kNone instead of 0)

--- CEffZFitter.cc
+++ CEffZFitter.cc
@@ -1171,7 +1171,7 @@ TGraphAsymmErrors* CEffZFitter::makeEffGraph(const std::vector<double> &edgesv,
     
     double eff, errl, errh;
     
-    if(fSigPass==CSignalModel::kNone && fSigFail==CBackgroundModel::kNone) {  // Cut-and-count
+    if(fSigPass==CSignalModel::kNone && fSigFail==CSignalModel::kNone) {  // Cut-and-count
       performCount(eff, errl, errh,
                    ibin,
 		   edgesv[ibin], edgesv[ibin+1],
